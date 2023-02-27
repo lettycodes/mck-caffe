5bf61c997954e33eaa1d1bf95d9b95be8d274227
Leticia <leticia_silva@mckinsey.com>
2023-02-27 10:03:33 -0300

    Editing ProductsController

--- src/main/java/mck/caffe/com/controller/ProductsController.java
+++ src/main/java/mck/caffe/com/controller/ProductsController.java
@@ -3,12 +3,13 @@ package mck.caffe.com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-/*import org.springframework.http.HttpStatus;*/
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import mck.caffe.com.model.Products;
 import mck.caffe.com.repository.ProductsRepository;
@@ -24,21 +25,21 @@ public class ProductsController {
 	@Autowired
 	private ProductsService productsService;
 	
-	@GetMapping("/products")
+	@GetMapping
 	public ResponseEntity<List<Products>> getAll() {
 		return ResponseEntity.ok(productsRepository.findAll());
 	}
 	
-	@GetMapping(value = "/{id}")
+	/*@GetMapping(value = "/{id}")
 	public ResponseEntity<Products> findById(@PathVariable Long id) {
 		Products obj = productsService.findById(id);
 		return ResponseEntity.ok().body(obj);
-	}
+	}*/
 	
-	/*@GetMapping("/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Products> getById(@PathVariable Long id) {
 		return productsRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-	}*/
+	}
 
 }
